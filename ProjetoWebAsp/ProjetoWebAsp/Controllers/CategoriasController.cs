4277f19e3c7a903fab7bf01cd0a17b5a0ef3886e
Leandro <Leandro@DESKTOP-6E04D66>
2018-05-22 20:26:22 -0300

    q

--- ProjetoWebAsp/ProjetoWebAsp/Controllers/CategoriasController.cs
+++ ProjetoWebAsp/ProjetoWebAsp/Controllers/CategoriasController.cs
@@ -11,7 +11,7 @@ namespace ProjetoWebAsp.Controllers
         {
 
            
-
+ 
            
 
             return View();
--- ProjetoWebAsp/ProjetoWebAsp/Models/Categoria.cs
+++ ProjetoWebAsp/ProjetoWebAsp/Models/Categoria.cs
@@ -9,7 +9,7 @@ namespace ProjetoWebAsp.Models
         public int CategoriaID { get; set; }
 
         [Display (Name = "Titulo")]//propriedade nome mostre titulo
-
+        [Required(ErrorMessage ="!!! Campo Obrigatorio !!! ")] // faz a validacao torna o campo obrigatotio
         public string Nome { get; set; }
 
         public bool Ativo { get; set; }
